47d42eadd44ca6b4ef4a3a3737e174bfdd605e69
danielpowell4 <danielpowell12@msn.com>
2016-01-20 18:59:47 -0700

    start of javascript files skeleton

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+/**
+  *
+  *   This grabs elements from the DOM
+  *
+  **/
+
+
+/**
+  *
+  *   This handles requests to Yelp
+  *
+  **/
+
+
+/**
+  *
+  *   This handles requests to Google Maps
+  *
+  **/
