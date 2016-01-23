f5138836bb377f70b337a9b98f7dc7d5ede85d7d
danielpowell4 <danielpowell12@msn.com>
2016-01-23 15:57:36 -0700

    scroll wheel to false

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,7 @@ function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 50.9995, lng: -118.2460},
-    //scrollwheel: false,
+    scrollwheel: false,
     zoom: 8
   });
 
