fc7362565c1114ac497f63d633dde564de11b441
danielpowell4 <danielpowell12@msn.com>
2016-03-20 01:04:06 -0600

    starting sticky map

--- scripts/app-engine.js
+++ scripts/app-engine.js
@@ -542,9 +542,20 @@ function reformatOnSize(){
     };
     scrollAdjustment = 240;
     map.setZoom(11);
+    createSticky(jQuery(".map-wrapper"));
   }
 };
 
+
+function createSticky(sticky) {
+	if (typeof sticky !== "undefined") {
+		var	pos = sticky.offset().top,
+				win = jQuery(window);
+		win.on("scroll", function() {
+    		win.scrollTop() >= pos ? sticky.addClass("fixed") : sticky.removeClass("fixed");
+		});
+	}
+}
 
 $( window ).resize(function() {
   reformatOnSize();
